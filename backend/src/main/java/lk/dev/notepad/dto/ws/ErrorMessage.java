356a2ecfc3f453f20455a9432e2f3e32cc1d0b50
mvinduwara <mvinduwara@gmail.com>
2026-06-13 13:56:31 +0530

    Create ErrorMessage.java

---
+++ backend/src/main/java/lk/dev/notepad/dto/ws/ErrorMessage.java
@@ -0,0 +1,15 @@
+package lk.dev.notepad.dto.ws;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorMessage {
+    private String code;
+    private String message;
+}
