c70230a49ba1df08c39c6de57d7257491ea128af
mvinduwara <mvinduwara@gmail.com>
2026-06-13 13:55:44 +0530

    Create AuthResponse.java

---
+++ backend/src/main/java/lk/dev/notepad/dto/auth/AuthResponse.java
@@ -0,0 +1,18 @@
+package lk.dev.notepad.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    private String token;
+    private String username;
+    private String email;
+    private Long userId;
+    private String color;
+}
