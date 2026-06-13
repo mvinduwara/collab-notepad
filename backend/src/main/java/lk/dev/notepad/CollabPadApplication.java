74760142c8a5a915ee87bca5a36f8e793b20b789
mvinduwara <mvinduwara@gmail.com>
2026-06-13 13:03:14 +0530

    new commit

---
+++ backend/src/main/java/lk/dev/notepad/CollabPadApplication.java
@@ -0,0 +1,11 @@
+package lk.dev.notepad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabPadApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CollabPadApplication.class, args);
+    }
+}
